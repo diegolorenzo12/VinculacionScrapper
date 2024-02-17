b4729768ca8e985f12fdf08bbaea72f6764afb39
Diego Lorenzo Villalobos <diegolorenzovillalobos@gmail.com>
2024-02-17 04:01:50 +0000

    added cors

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { GetStudentInfo, capitalizeFirstLetter } from "./models/Student"
+import cors from 'cors'; 
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/getStudent', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required.' });
+  }
+
+  try {
+    const studentInfo = await GetStudentInfo(username, password);
+
+    if (studentInfo) {
+        if(studentInfo.FirstName === null){
+            res.status(400).json({ error: 'Incorrect Credentials or student not found.' });
+        }else{
+            res.status(200).json(studentInfo);
+        }
+    } else {
+      res.status(404).json({ error: 'Student information not found.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+
+app.get('/', async (req: Request, res: Response) => {
+  return res.status(200).json({ message: 'hola' });
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
